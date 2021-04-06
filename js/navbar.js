710a2c09b56394edc64783c20c68f45c9c163479
dmytro <tedi4t@gmail.com>
2021-04-06 17:35:54 +0300

    imporoved activating secting when scrolling for last section

--- js/navbar.js
+++ js/navbar.js
@@ -6,31 +6,54 @@ $('nav.navbar .navbar-collapse').click(function() {
   $('nav.navbar .navbar-collapse-items').toggleClass('unactive');
 })
 
+// coef of covering for activating last section
+const lastSectionKoef = 8 / 10;
+
 // navbar scroll activate items
 const sections = $('section');
 let currentActiveSection;
 
-function activateSection() {
-  const preOffset = 100;
-  const currentOffset = $(document).scrollTop() + preOffset;
-  sections.each(function() {
-    const sectionOffset = $(this).offset().top;
-    const sectionHeight = $(this).innerHeight();
-    const sectionBottom = sectionOffset + sectionHeight;
-    const sectionInView = 
-      sectionOffset < currentOffset && currentOffset < sectionBottom;
-    const currentSectionId = $(this).attr('id');
-
-    if (sectionInView && currentActiveSection !== currentSectionId) {
-      currentActiveSection = currentSectionId;
-      $('nav.navbar a').removeClass('active');
-      $(`nav.navbar a[href="#${currentSectionId}"]`).addClass('active');
-    }
-  })
+function activateSection(sectionId) {
+  currentActiveSection = sectionId;
+  $('nav.navbar a').removeClass('active');
+  $(`nav.navbar a[href="#${sectionId}"]`).addClass('active');
 }
 
-activateSection();
-$(window).scroll(activateSection);
+function activateSectionOnScroll() {
+  const preOffset = 100;
+  const currentOffset = $(document).scrollTop() + preOffset;
+  
+  //check last section at first
+  const lastSection = sections.last();
+  const lastSectionOffsetTop = lastSection.offset().top;
+  const lastSectionHeight = lastSection.innerHeight();
+  const windowHeight = $(window).innerHeight();
+  const lastSectionId = lastSection.attr('id');
+  
+  const lastSectionTooSmall = lastSectionHeight < windowHeight;
+  const lastSectionInView = lastSectionOffsetTop < currentOffset + windowHeight - lastSectionHeight * lastSectionKoef;
+  
+  if (lastSectionTooSmall && lastSectionInView) {
+    activateSection(lastSectionId);
+  } else {
+    sections.each(function() {
+      const sectionOffset = $(this).offset().top;
+      const sectionHeight = $(this).innerHeight();
+      const sectionBottom = sectionOffset + sectionHeight;
+      const sectionInView = 
+        sectionOffset < currentOffset && currentOffset < sectionBottom;
+
+      const currentSectionId = $(this).attr('id');
+
+      if (sectionInView && currentActiveSection !== currentSectionId) {
+        activateSection(currentSectionId);
+      }
+    })
+  }
+}
+
+activateSectionOnScroll();
+$(window).scroll(activateSectionOnScroll);
 
 const navHeight = $('nav.navbar').innerHeight();
 
